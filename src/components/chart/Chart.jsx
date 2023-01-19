bcf4b231286b7f549211c2a015e6a27c4d017e3d
MarwanOladele <oladelemarwan@gmail.com>
2023-01-19 12:38:33 +0100

    first commit

---
+++ src/components/chart/Chart.jsx
@@ -0,0 +1,9 @@
+import "./Chart.css"
+
+const Chart = () => {
+  return (
+    <div className="chart">chart</div>
+  )
+}
+
+export default Chart
--- src/components/featuredInfo/FeaturedInfo.jsx
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -10,7 +10,18 @@ const FeaturedInfo = () => {
         <div className="featured_money_container">
           <span className="featured_money">$2,410</span>
           <span className="featured_rate">
-            -11.4 <ArrowDownwardIcon />
+            -11.4 <ArrowDownwardIcon className="featured_icon negative" />
+          </span>
+        </div>
+        <span className="feature_sub">Compared to last month</span>
+      </div>
+
+      <div className="featured_item">
+        <span className="featured_title">Sales</span>
+        <div className="featured_money_container">
+          <span className="featured_money">$5,648</span>
+          <span className="featured_rate">
+           -2.4 <ArrowDownwardIcon  className="featured_icon negative" />
           </span>
         </div>
         <span className="feature_sub">Compared to last month</span>
@@ -21,7 +32,7 @@ const FeaturedInfo = () => {
         <div className="featured_money_container">
           <span className="featured_money">$2,348</span>
           <span className="featured_rate">
-           +1.4 <ArrowUpwardIcon />
+           +1.4 <ArrowUpwardIcon  className="featured_icon" />
           </span>
         </div>
         <span className="feature_sub">Compared to last month</span>
